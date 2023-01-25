547d5792954475dcb8e464a332ef31b8fc39a88a
Nida Ekinci <116806730+NidaEkinci@users.noreply.github.com>
2023-01-25 22:57:44 +0300

    Sum Of Multiples
    The sum of all the multiples of 3 or 5 below 1000.

--- PE1.c
+++ PE1.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 main()
 {
-    int i=0,t=0;
-    while(i<1000)
+    int i=0, sum=0;
+    while(i < 1000)
     {
-        if(i%3==0)
+        if(i % 3 == 0)
         {
-           printf("%d\n",i);
-           t=t+i; 
+           printf("Multiples of 3 : %d\n", i);
+           sum += i; 
         }
-        else if(i%5==0)
+        else if(i % 5 == 0)
         {
-            printf("%d\n",i);
-            t=t+i;
+            printf("Multiples of 5 : %d\n", i);
+            sum += i;
         }
         i++;
     }
-    printf("Toplamlari:%d",t);
+    printf("Multiples of 3 or 5 : %d", sum);
 }
