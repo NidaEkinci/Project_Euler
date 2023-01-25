5bf99c4388b8a05fede26ad6233289d1869bdf9d
Nida Ekinci <116806730+NidaEkinci@users.noreply.github.com>
2023-01-25 15:59:58 +0300

    Add files via upload
    The largest palindrome made from the product of two 3-digit numbers.

---
+++ PE4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+main()
+{
+	int i, j, original ,product, remainder, reserved, max=0;
+	for(i=999 ; i>=100 ; i--)
+	{
+		for(j=999 ; j>=100 ; j--)
+		{
+			product = i*j;
+			remainder=0; reserved=0;
+			while (product != 0)
+	    	{
+				remainder = product % 10;
+				reserved = reserved * 10 + remainder;
+				product /= 10;
+			}
+			original = i*j;
+			if (original == reserved && original > max)
+			{
+				max = original;	
+			}
+		}
+	}
+	printf ("%d is a polindrome.", max);
+}
