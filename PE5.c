9d6dd1bd0825730c33eb0b75236047b817cfb96d
Nida Ekinci <116806730+NidaEkinci@users.noreply.github.com>
2023-01-29 16:39:15 +0300

    Smallest Multiple
    The smallest positive number that is evenly divisible by all of the numbers from 1 to 20.

---
+++ PE5.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+main()
+{
+	unsigned long long int j, smallest_multiple, product=1;
+	int i, test;
+	for(i=1 ; i<=20 ; i++) product = product * i;
+	for(j=20 ; j <= product ; j++)
+	{
+		test = 1; i = 1;
+		while(test == 1 && i <= 20)
+		{
+			if(j % i != 0) test = 0;
+			else i++;
+		}
+		if(test == 1)
+		{
+			smallest_multiple = j;
+			break;
+		}
+	}
+	printf("EKOK : %llu", smallest_multiple);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
